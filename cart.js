e2da3d0dbec9b101af877e46b5d6fd6f009c40a0
your name <65359948+hamidray@users.noreply.github.com>
2021-02-10 21:57:46 -0500

    commit

--- cart.js
+++ cart.js
@@ -37,164 +37,37 @@
 
 displayCart();
 
- ///// createContact = (response) => {
-       /// console.log(response);
-       
-        const container = document.getElementsByClassName('container');
-        const form = document.getElementsByClassName('card-p5');
-        const firstname = document.getElementById('firstname');
-        const lastname = document.getElementById('lastname');
-        const email = document.getElementById('email');
-        const address = document.getElementById('address');
-        const submitButton =  document.getElementById('submit-button');
-        const remove = document.querySelectorAll('text-danger.d-none');
-        const submitError = document.createElement('div');
-        
-        
-        ///boolen to false/////
-        let isFirstnameValid= false;
-        let isLastnameValid= false;
-        let isValidAddress= false;
-        let isEmailValid= false;
 
+let contacts = [];
+const addContact = (ev)=>{
+  ev.preventDefault();
+let contact = {
+  firstname: document.getElementById('firstname').value,
+  lastname: document.getElementById('lastname').value,
+  email: document.getElementById('email').value,
+  address: document.getElementById('address').value,
+}
+contacts.push(contact);
+document.forms[0].reset(); ////to clear the form for the next entries
 
-        const regLastname = /^ [A-Za-z] {3,32}$/; 
-        const regName = /^ [A-Za-z] {3,32}$/;
-        const regAddress = /^ [A-Za-z0-9 ] {7,32}$/; 
-        const emailReg = /^ [a-z0-9._%+-] +@[a-z0-9.-]+\.[a-z]{2,}$/;
-           
-    
+///for display purposes only
 
-       
-        firstname.addEventListener('blur', () => {
-            if (regName.test(firstname.value)) {
-                firstnameError.classList.add('d-none');
-                isFirstnameValid = true;
-                firstname.style.border = 'medium solid red';
-            }
-            else {
-                firstnameError.classList.remove('d-none');
-                firstname.style.border = 'medium solid orang';
-            }
-        }); 
+console.warn('Your INFO Added' ,{contacts} );
 
-        lastname.addEventListener('blur', () => {
-            if (regLastname.test(lastname.value)) {
-                lastnameError.classList.add('d-none');
-                isLastnameValid = true;
-                lastname.style.border = 'medium solid red';
-            }
-            else {
-                lastnameError.classList.remove('d-none');
-                lastname.style.border = 'medium solid orang';
-            }
-        });
+///pre.textContent = JSON.stringify('Submited', contact);
 
-        email.addEventListener('blur', ($event) => {
-            if (emailReg.test(email.value)) {
-                emailError.classList.add('d-none');
-                isEmailValid = true;
-                email.style.border = 'medium solid red';
-            }
-            else {
-                emailError.classList.remove('d-none');
-                email.style.border = 'medium solid orang';
-            }
-        });
+///////saving to local storage/////////
 
-        address.addEventListener('blur', () => {
-            if (regAddress.test(address.value)) {
-                addressError.classList.add('d-none');
-                isValidAddress = true;
-                address.style.border = 'medium solid red';
-            }
-            else {
-                addressError.classList.remove('d-none');
-                address.style.border = 'medium solid orang';
-            }
-        });
-         
-          submitButton.addEventListener('click' , ($event) => {
-               $event.preventDefault();
-               const Contact = {
-                   firstName:firstname.value,
-                   lastName: lastname.value,
-                   address: address.value,
-                   email: email.value,
-                
-               }
-                        const orderObject = {
-                Contact , container
-            };
-                  /////submitForm() Validation Bool/////
+localStorage.setItem('MyContactList' , JSON.stringify(contacts))
+};
+          document.addEventListener('DOMContentLoaded' , () => {
+  document.getElementById('btn').addEventListener('click' , addContact);
+  
 
-           if ((isFirstnameValid) && (isLastnameValid) && (isEmailValid) && (isValidAddress)) {
-                submitForm(orderObject);
-                
-           }
-               //if an ERROR!!!!!!///
-           else {
-               
-               document.getElementsByTagName('submitError')
-            }
+     });
 
-           if (firstname.value === '') {
-               firstnameError.classList.remove('text-danger');
-               firstname.style.border = 'medium solid green';
-            }
-
-            if (lastname.value === '') {
-               lastnameError.classList.remove('text-danger');
-               lastname.style.border = 'medume solid green';
-            }
-             if (address.value === '') {
-                addressError.classList.remove('text-danger');
-              address.style.border = 'medium solid green';
-            }
-               
-             
-            if (email.value === '') {
-              emailError.classList.remove('text-danger');
-               email.style.border = 'medium solid green';
-           }
-            
-
-        });
-            
-        submitForm = async (orderObject) => {
-           try {
-                const resquestPromise = makeRequest(orderObject);
-                const response = await resquestPromise;
-
-               displayConfirmation(response);
-                createContact(response); 
-           } catch (error) {
-                document.querySelectorAll('form').innerHTML = '<h2 class = "mx-auto">' + error +'<h2>';
-           }
-        }
-                             /////Function to make api request////
-                            
-                            makeRequest = (data) => {
-                               return new Promise((resolve, reject) => {
-                                    let apiRequest = new XMLHttpRequest();
-                                    apiRequest.open('post' ,'http://localhost:3000/api/cameras/order');
-                                    apiRequest.setRequestHeader('Content-Type', 'application/json');
-                                    apiRequest.send(JSON.stringify(data));
-                                   apiRequest.onreadystatechange = () => {
-                                        if (apiRequest.readyStatus === 4) {
-                                           if (apiRequest.status === 201) {
-                                                resolve(JSON.parse(apiRequest.response));
-                                           }
-                                            if (apiRequest.status === 400) {
-                                                reject(" Something Went Wrong -- API Request Failed!!")
-                                        }
-                                    }
-                                };
-                           });
-                        }
-                            
-
-                      ///close response//////
+ 
 
 
 
+ 
