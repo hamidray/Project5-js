65a44c9240f36fc530d8356c46ec29a29cc2363a
your name <65359948+hamidray@users.noreply.github.com>
2021-02-11 13:26:12 -0500

    first commit

--- cart.js
+++ cart.js
@@ -1,8 +1,10 @@
-       function displayCart(){
+      ///////////Show Item ////////////////////////////
+let cartItems = localStorage.getItem("cart");
+cartItems = JSON.parse(cartItems);
+console.log(cartItems)
     
-    let cartItems = localStorage.getItem("cart");
-    cartItems = JSON.parse(cartItems);
-    console.log(cartItems)
+function displayCart(){
+    
     let productContainer = document.querySelector(".products");
      let cartCost = localStorage.getItem('totalCost');
     console.log(cartItems);
@@ -11,18 +13,52 @@
         productContainer.innerHTML = '';
         Object.values(cartItems).map(item => {
             productContainer.innerHTML += `
-              <div class ="product">
-              
-              <ion-icon name ="close-circle"></ion-icon>
+           <div class ="product">
+                          
               <div class="product-title">${item.name}</div>
               <div class="lens">${item.lenses}</div>
               <div class="price">$${item.price}</div>
-              
-              </div>
+              <button class="remove" data-name="${item.name}" data-lens="${item.lenses}">Remove</button>
+
+           </div>
             
           `
+          productContainer.onclick = function(e) {
+            console.log("clicked List")
+            console.log(e.target)
+             const lens = e.target.dataset.lens
+             const name = e.target.dataset.name
+              removeItem(e,name,lens);
+              
+              return
+  
+         //   }
+          }
+        
         });
+
+//////////////////////Remove Item/////////////////////////////////
+
+        
+        function removeItem(e,name,lens) {
+         
+          //need to delete item from cart//
+          for (let i = 0; i <cartItems.length; i++) {
+            if (cartItems[i].name === name && cartItems[i].lenses === lens) {
+              cartItems.splice(i, 1);
+              console.log(cartItems);
             
+           }
+          }
+          //remove node from page
+          e.target.parentElement.remove();
+
+          //get cart sync in local storage
+          localStorage.setItem("cart" , JSON.stringify(cartItems));
+          cartItems = JSON.parse(localStorage.getItem("cart"));
+          
+        
+        }
              productContainer.innerHTML += `
         <div class="basketTotalContainer">
         <h5 class="basketTotalTitle">
@@ -34,11 +70,15 @@
         `
     }
   }
-
+  
 displayCart();
 
 
+
+///////////////////// ADD Contacat////////////////
+
 let contacts = [];
+let form = document.getElementsByClassName('card-p5');
 const addContact = (ev)=>{
   ev.preventDefault();
 let contact = {
@@ -58,7 +98,7 @@ console.warn('Your INFO Added' ,{contacts} );
 
 ///////saving to local storage/////////
 
-localStorage.setItem('MyContactList' , JSON.stringify(contacts))
+///localStorage.setItem('MyContactList' , JSON.stringify(contacts))
 };
           document.addEventListener('DOMContentLoaded' , () => {
   document.getElementById('btn').addEventListener('click' , addContact);
